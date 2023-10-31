2787ce09ce3e78176b47edb4f6d93aef167c0a0f
Eric Filser <filser.eric@protonmail.com>
2023-10-31 11:29:35 +0100

    Add waitFor with 5s timeout to be sure the msg is rendered

--- src/pages/Home/index.test.js
+++ src/pages/Home/index.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Home from "./index";
 
 describe("When Form is created", () => {
@@ -21,7 +21,7 @@ describe("When Form is created", () => {
         })
       );
       await screen.findByText("En cours");
-      await screen.findByText("Message envoyé !");
+      await waitFor(() => screen.findByText("Message envoyé !"), { timeout: 5000 });
     });
   });
 
