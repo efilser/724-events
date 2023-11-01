eeb7f4c1dad561ac6540b55031773bbe37b6485d
Eric Filser <filser.eric@protonmail.com>
2023-11-01 16:12:28 +0100

    Update getMonth const + update the use of useData

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,8 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const {data} = useData()
+  const last = data?.events?.[data.events.length - 1];
   return <>
     <header>
       <Menu />
